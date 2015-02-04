e011bfbed5998b8676910ad56cc73f2ce0027496
Tiago Zimmermann <tiagozimmermann88@gmail.com>
2015-02-04 16:37:16 -0200

    Melhoria na barra de serviços

--- modules/default/controllers/SystemstatusController.php
+++ modules/default/controllers/SystemstatusController.php
@@ -17,6 +17,7 @@
  */
 
 require_once 'Zend/Controller/Action.php';
+require_once "includes/AsteriskInfo.php";
 require_once 'includes/functions.php';
 
 
@@ -36,14 +37,15 @@ class SystemstatusController extends Zend_Controller_Action {
 
         //Disables layout so jQuery .get() can call pure HTML at the index page.
         $this->_helper->layout()->disableLayout();
-
-        $this->view->breadcrumb = $this->view->translate("Welcome to Snep version %s", SNEP_VERSION);
+        
+        $auth = Zend_Auth::getInstance();
+        $username = $auth->getIdentity();
+        $this->view->breadcrumb = $this->view->translate("Welcome to Snep, ") . $username ;
 
         // Direcionando para o "snep antigo"
         $config = Zend_Registry::get('config');
         $db = Zend_Registry::get('db');
 
-        // GOD!
         $linfoData = new Zend_Http_Client('http://localhost/' . str_replace("/index.php", "", $this->getFrontController()->getBaseUrl()) . '/lib/linfo/index.php?out=xml');
         try {
             $linfoData->request();
@@ -58,27 +60,26 @@ class SystemstatusController extends Zend_Controller_Action {
         } else {
 
             $systemInfo = array();
-            $uptimeRaw = explode(';', $sysInfo->core->uptime);
-            $systemInfo['uptime'] = $uptimeRaw[0];
+            $execUptimeRaw = explode(",", exec("uptime"));
+            $uptimeRaw = substr($execUptimeRaw[0], strpos($execUptimeRaw[0], "up") + 2);
 
-
-            require_once "includes/AsteriskInfo.php";
+            if (strpos($uptimeRaw, "min") > 0) {
+                $systemInfo['uptime'] = substr($uptimeRaw, 0, strpos($uptimeRaw, "min") + 3);
+            } elseif (strpos($uptimeRaw, ":") > 0) {
+                $uptimeTmp = explode(":", $uptimeRaw);
+                $systemInfo['uptime'] = $uptimeTmp[0] . $this->view->translate(' hour(s), ') . $uptimeTmp[1] . $this->view->translate(' minutes');
+            } else {
+                $systemInfo['uptime'] = substr($uptimeRaw, 0, strpos($uptimeRaw, "day")) . $this->view->translate(' dias, ');
+                $uptimeTmp = explode(":", $execUptimeRaw[1]);
+                $systemInfo['uptime'].= $uptimeTmp[0] . $this->view->translate(' hour(s), ') . $uptimeTmp[1] . $this->view->translate(' minutes');
+            }
+            $systemInfo['uptime'] = trim($systemInfo['uptime']);
 
             try {
                 $astinfo = new AsteriskInfo();
                 $astVersionRaw = explode('@', $astinfo->status_asterisk("core show version", "", True));
                 preg_match('/Asterisk (.*) built/', $astVersionRaw[0], $astVersion);
 
-                $data = $astinfo->status_asterisk("database show", "", True);
-                $lines = explode("\n", $data);
-                $arr = array();
-
-                $systemInfo['sip_peers'] = str_replace(array("onitored","line","sip"), array("onit.","",""), 
-                        $astinfo->status_asterisk("sip show  peers", "sip peers"));    
-                $systemInfo['sip_channels'] = $astinfo->status_asterisk("sip show channels", "SIP dialog");
-                $systemInfo['iax2_peers'] = str_replace(array("onitored","line","iax2"), array("onit.","",""), 
-                        $astinfo->status_asterisk("iax2 show peers", "iax2 peers"));
-                $systemInfo['agents'] = $astinfo->status_asterisk("show agents", "agents configured");
             } catch (Exception $e) {
                 
             }
@@ -89,16 +90,23 @@ class SystemstatusController extends Zend_Controller_Action {
             }
             $systemInfo['mysql'] = trim(exec("mysql -V | awk -F, '{ print $1 }' | awk -F'mysql' '{ print $2 }'"));
 
-            Zend_Date::setOptions(array('extend_month' => true));
-            $dataAtual = new Zend_Date();
-            $systemInfo['data'] = $dataAtual->toString('dd/MM/YYYY hh:mm:ss');
+            if (file_exists("/etc/slackware-version")) {
+                exec("cat /etc/slackware-version", $linuxVer);
+                $systemInfo['linux_ver'] = $linuxVer[0];
+            } else {
+                exec("cat /etc/issue", $linuxVer);
+                $systemInfo['linux_ver'] = substr($linuxVer[0], 0, strpos($linuxVer[0], "\\"));
+            }
 
-            $systemInfo['linux_ver'] = $sysInfo->core->os . ' / ' . $sysInfo->core->Distribution;
+            $systemInfo['linux_kernel'] = exec("uname -sr");
 
-            $systemInfo['linux_kernel'] = $sysInfo->core->kernel;
+            $hard1 = exec("cat /proc/cpuinfo | grep name |  awk -F: '{print $2}'");
+            $hard2 = exec("cat /proc/cpuinfo | grep MHz |  awk -F: '{print $2}'");
+            $systemInfo['hardware'] = trim($hard1 . " , " . $hard2 . " Mhz");
 
-            $cpuRaw = explode('-', $sysInfo->core->CPU);
-            $systemInfo['hardware'] = $cpuRaw[1];
+            $hard1 = exec("cat /proc/cpuinfo | grep name |  awk -F: '{print $2}'");
+            $hard2 = exec("cat /proc/cpuinfo | grep MHz |  awk -F: '{print $2}'");
+            $systemInfo['hardware'] = trim($hard1 . " , " . $hard2 . " Mhz");
 
             $cpuNumber = count(explode('<br />', $sysInfo->core->CPU));
 
@@ -107,22 +115,11 @@ class SystemstatusController extends Zend_Controller_Action {
             
             $config = Zend_Registry::get('config');
           
-            if (isset($config->ambiente->path_voz)) {
-                $path_voz = $config->ambiente->path_voz;
-            } else {
-                $path_voz = "";
-            }
+            
             unset($config);
             
-            $systemInfo['num_arqvoz'] = exec("scripts/num_arquivos " . $path_voz);
-           // $systemInfo['spc_arqvoz'] = exec("du -sch $path_voz | cut -f1");
-
             $systemInfo['usage'] = round(($loadAvarege * 100) / ($cpuNumber - 1));
-
-
             $systemInfo['memory'] = self::sys_meminfo();
-
-
 
             $systemInfo['memory']['swap'] = array(
                 'total' => $this->byte_convert(floatval($sysInfo->memory->swap->core->free)),
@@ -141,6 +138,22 @@ class SystemstatusController extends Zend_Controller_Action {
                 );
             }
 
+            $sqlN = "select count(*) from";
+            $select = $db->query($sqlN . ' peers');
+            $result = $select->fetch();
+
+            $systemInfo['num_peers'] = $result['count(*)'];
+
+            $select = $db->query($sqlN . ' trunks');
+            $result = $select->fetch();
+
+            $systemInfo['num_trunks'] = $result['count(*)'];
+
+            $select = $db->query($sqlN . ' regras_negocio');
+            $result = $select->fetch();
+
+            $systemInfo['num_routes'] = $result['count(*)'];
+
             $netArray = $sysInfo->net->interface;
             $count = 0;
 
@@ -153,22 +166,6 @@ class SystemstatusController extends Zend_Controller_Action {
                 }
             }
 
-//            $sqlN = "select count(*) from";
-//            $select = $db->query($sqlN . ' peers');
-//            $result = $select->fetch();
-//
-//            $systemInfo['num_peers'] = $result['count(*)'];
-//
-//            $select = $db->query($sqlN . ' trunks');
-//            $result = $select->fetch();
-//
-//            $systemInfo['num_trunks'] = $result['count(*)'];
-//
-//            $select = $db->query($sqlN . ' regras_negocio');
-//            $result = $select->fetch();
-//
-//            $systemInfo['num_routes'] = $result['count(*)'];
-
             $systemInfo['modules'] = array();
             $modules = Snep_Modules::getInstance()->getRegisteredModules();
             foreach ($modules as $module) {
@@ -179,35 +176,8 @@ class SystemstatusController extends Zend_Controller_Action {
                 );
             }
 
-            //verifica se ler_queues está rodando
-            $cc = self::ler_queues();
-            
-            $systemInfo['cc'] = $cc["exists"];
-            if($systemInfo['cc'] == true){
-                $systemInfo['cc'] = $cc['ler_queues'];
-            }
-            
             $this->view->indexData = $systemInfo;
 
-            // Creates Snep_Inspector Object
-            $objInspector = new Snep_Inspector();
-
-            // Get array with status of inspected system requirements
-            $inspect = $objInspector->getInspects();
-
-            // Verify errors
-            $this->view->error = false;
-            foreach ($inspect as $log => $message) {
-                if ($message['error'] == 1) {
-                    $this->view->error = true;
-                }
-            }
-
-            // Inspector url
-            $this->view->inspector = $this->getFrontController()->getBaseUrl() . '/inspector/';
-
-            //Sends inspects to be viewed on the services screen (footer)
-            $this->view->inspect = $inspect;
         }
     }
     
@@ -227,39 +197,11 @@ class SystemstatusController extends Zend_Controller_Action {
         // Get the notation
         $notation = 1024;
 
-        // Fixes large disk size overflow issue
-        // Found at http://www.php.net/manual/en/function.disk-free-space.php#81207
         $types = array('B', 'KB', 'MB', 'GB', 'TB');
         $types_i = array('B', 'KiB', 'MiB', 'GiB', 'TiB');
         for ($i = 0; $size >= $notation && $i < (count($types) - 1 ); $size /= $notation, $i++)
             ;
         return(round($size, $precision) . ' ' . ($notation == 1000 ? $types[$i] : $types_i[$i]));
-    }
-    
-    /**
-     * ler_queues - Verify if daemon it is ative
-     * @return <boolean>
-     */
-    function ler_queues() {
-        $output = shell_exec('ps aux | grep ler_que');
-
-        $findme = 'ler_queues.php';
-        $pos = strpos($output, $findme);
-
-        if (class_exists("Cc_AgentsInfo") || class_exists("Cc_Statistical")) {
-            if ($pos === false) {
-                $cc['ler_queues'] = "Desativado";
-                $cc['exists'] = true;
-                return $cc;
-            } else {
-                $cc['ler_queues'] = "Ativado";
-                $cc['exists'] = true;
-                return $cc;
-            }
-        } else {
-            $cc['exists'] = false;
-            return $cc;
-        }
     }
     
     /**

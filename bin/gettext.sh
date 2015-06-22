b8bc4126f14f656d0509bc02e36becbbced80d60
root <root@debian.opens.com.br>
2015-06-22 09:11:15 -0300

    Ajustes arquivos de traducao

--- bin/gettext.sh
+++ bin/gettext.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/sh -x
 
 echo "" > messages.po
-find $1 -type f \( -name "*.php" -o -name "*.phtml" \) | grep -v ".svn" | grep -v "Zend/" | xgettext --keyword=translate --language=PHP --from-code=utf-8 -j -f -;
+echo "Fase 1 : Arquivos php/phtml"
+find $1 -type f \( -name "*.php" -o -name "*.phtml" \) | grep -v ".git" | grep -v "Zend/" | xgettext --keyword=translate --language=PHP --from-code=utf-8 -j -f -;
 echo "" >> messages.po;
+echo "Fase 2 : Arquivos xml"
 export PYTHONIOENCODING=utf_8;
-for file in `find $1 -type f -name *.xml | grep -v .svn | grep -v Zend/`; do ./findstrings.py -f $file >> messages.po; done;
+for file in `find $1 -type f -name *.xml | grep -v .git | grep -v Zend/`; do python findstrings.py -f $file >> messages.po; done;
 xgettext -s messages.po

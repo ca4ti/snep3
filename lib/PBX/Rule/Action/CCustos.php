268f7f66bd273a5a49e6b018f2d4718a26f3d653
Douglas Conrad <conrad@opens.com.br>
2015-07-02 13:01:53 -0300

    Adicionado lib da acao de DiscarRamal e Centro de Custos que sao acionadas na Regra de Negócio e apresentavam erro de Broken pipe no console do asterisk.

---
+++ lib/PBX/Rule/Action/CCustos.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ *  This file is part of SNEP.
+ *  Para território Brasileiro leia LICENCA_BR.txt
+ *  All other countries read the following disclaimer
+ *
+ *  SNEP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  SNEP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SNEP.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
+
+/**
+ * Setar Centro de Custos.
+ *
+ * Ação das regras do snep que define um centro de cusos para classificar a
+ * ligação.
+ *
+ * @category  Snep
+ * @package   PBX_Rule_Action
+ * @copyright Copyright (c) 2010 OpenS Tecnologia
+ * @author    Henrique Grolli Bassotto
+ */
+class PBX_Rule_Action_CCustos extends PBX_Rule_Action {
+
+    /**
+     * @var Internacionalização
+     */
+    private $i18n;
+
+    /**
+     * Construtor
+     * @param array $config configurações da ação
+     */
+    public function __construct() {
+        $this->i18n = Zend_Registry::get("i18n");
+    }
+
+    /**
+     * Retorna o nome da Ação. Geralmente o nome da classe.
+     *
+     * @return Nome da Ação
+     */
+    public function getName() {
+        return $this->i18n->translate("Definir Centro de Custos");
+    }
+
+    /**
+     * Retorna o numero da versão da classe.
+     *
+     * @return Versão da classe
+     */
+    public function getVersion() {
+        return "1.0";
+    }
+
+    /**
+     * Seta as configurações da ação.
+     *
+     * @param array $config configurações da ação
+     */
+    public function setConfig($config) {
+        $this->config = $config;
+    }
+
+    /**
+     * Retorna uma breve descrição de funcionamento da ação.
+     * @return Descrição de funcionamento ou objetivo
+     */
+    public function getDesc() {
+        return $this->i18n->translate("Define um centro de custos para classificação da ligação");
+    }
+
+    /**
+     * Devolve um XML com as configurações requeridas pela ação
+     * @return String XML
+     */
+    public function getConfig() {
+        $ccustos = (isset($this->config['ccustos']))?"<value>{$this->config['ccustos']}</value>":"";
+
+        return <<<XML
+<params>
+    <ccustos>
+        <id>ccustos</id>
+        $ccustos
+    </ccustos>
+</params>
+XML;
+    }
+
+    /**
+     * Executa a ação. É chamado dentro de uma instancia usando AGI.
+     *
+     * @param AGI $asterisk
+     * @param int $rule - A regra que chamou essa ação. É passado pra que
+     * a ação possa restaurar as configurações dela para essa regra. Esse parametro
+     * á opcional.
+     */
+    public function execute($asterisk, $request) {
+        $log = Zend_Registry::get('log');
+
+        $log->info("Definindo centro de custos para {$this->config['ccustos']}.");
+        $asterisk->set_variable('CDR(accountcode)', $this->config['ccustos']);
+    }
+}

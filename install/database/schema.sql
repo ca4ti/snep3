10883f2fa098d4172c7329d895a5a0820c9f9a12
Tiago Zimmermann <tiago.zimmermann@opens.com.br>
2016-04-06 10:06:32 -0300

    Add multiples extensions and update sql for version snep-rc3

--- install/database/schema.sql
+++ install/database/schema.sql
@@ -759,6 +759,7 @@ CREATE TABLE IF NOT EXISTS `core_notifications` (
 -- Table structure for table `core_config`
 --
 CREATE TABLE IF NOT EXISTS core_config (
+    `config_module` VARCHAR (255) NOT NULL,
     `config_name` VARCHAR (255) NOT NULL,
     `config_value` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- install/database/update/3.0/update.sql
+++ install/database/update/3.0/update.sql
@@ -1,7 +1,9 @@
 --
 -- Table structure for table `core_config`
 --
-CREATE TABLE IF NOT EXISTS core_config (
+CREATE TABLE IF NOT EXISTS `core_config` (
+    `config_module` VARCHAR (255) NOT NULL,
     `config_name` VARCHAR (255) NOT NULL,
     `config_value` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+

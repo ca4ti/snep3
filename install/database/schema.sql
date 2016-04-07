bed9be5a03e0c29039c46938001c352b353a6487
Tiago Zimmermann <tiago.zimmermann@opens.com.br>
2016-04-07 16:28:57 -0300

    New functionality of centralization of module settings

--- install/database/schema.sql
+++ install/database/schema.sql
@@ -758,11 +758,13 @@ CREATE TABLE IF NOT EXISTS `core_notifications` (
 --
 -- Table structure for table `core_config`
 --
-CREATE TABLE IF NOT EXISTS core_config (
-    `config_module` VARCHAR (255) NOT NULL,
-    `config_name` VARCHAR (255) NOT NULL,
-    `config_value` VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `core_config` (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `config_module` VARCHAR (255) NOT NULL,
+  `config_name` VARCHAR (255) NOT NULL,
+  `config_value` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 --

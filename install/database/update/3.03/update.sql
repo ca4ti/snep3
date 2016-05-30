5467c7cd02812474a62bbb82bfc4459270a610c2
Flavio H Somensi <fsomensi@gmail.com>
2016-05-30 14:49:33 -0300

    Ajustes string traducao ; AJustes dados sql.

--- install/database/update/3.03/update.sql
+++ install/database/update/3.03/update.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `peers` ADD COLUMN `useragent` VARCHAR(250) DEFAULT NULL;
 ALTER TABLE `queues` ADD COLUMN `ringinuse` BOOL NOT NULL DEFAULT '1';
 
-INSERT INTO `core_config` (`config_module`, `config_name`, `config_value`) VALUES ("default","userfield","TSAAMMDDHHiiSRDS"),("default","userfield_ud","");
+INSERT INTO `core_config` (`config_module`, `config_name`, `config_value`) VALUES ("default","userfield","TS_AAMMDD_HHii_SR_DS"),("default","userfield_ud","");
 
 UPDATE `core_config` set `config_module`= 'default' WHERE `config_module`= 'CORE' ;
 UPDATE `core_config` set `config_name`= 'host_notification' WHERE `config_name`= 'HOST_NOTIFICATION' AND `config_module`= 'default';
